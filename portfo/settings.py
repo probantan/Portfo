ed72eca9f97c9bde009873c81fe1e1295b2a36d8
protus <protus.bantan@gmail.com>
2019-06-26 17:40:07 +0300

    Initial commits

--- portfo/settings.py
+++ portfo/settings.py
@@ -25,10 +25,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 
 # Application definition
@@ -80,7 +83,6 @@ WSGI_APPLICATION = 'portfo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# development
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
@@ -102,7 +104,6 @@ else:
    }
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
